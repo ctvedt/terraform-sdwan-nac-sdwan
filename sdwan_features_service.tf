1902ffcdf8e2b5e8635239e91eb04a5c4946afd3
ctvedt <christopher.tvedt@gmail.com>
2025-07-23 09:06:07 +0200

    Add partial BGP support for UX2.0 transport and service VPN

--- sdwan_features_service.tf
+++ sdwan_features_service.tf
@@ -560,3 +560,135 @@ resource "sdwan_service_lan_vpn_interface_ethernet_feature" "service_lan_vpn_int
   xconnect                                       = try(each.value.interface.gre_tloc_extension_xconnect, null)
   xconnect_variable                              = try("{{${each.value.interface.gre_tloc_extension_xconnect_variable}}}", null)
 }
+
+resource "sdwan_service_routing_bgp_feature" "service_routing_bgp_feature" {
+  for_each = {
+    for bgp_item in flatten([
+      for profile in try(local.feature_profiles.service_profiles, []) : [
+        for bgp in try(profile.bgp_features, []) : {
+          profile = profile
+          bgp = bgp
+        }
+      ]
+    ])
+    : "${bgp_item.profile.name}-${bgp_item.bgp.name}" => bgp_item
+  }
+  name                              = each.value.bgp.name
+  always_compare_med                = try(each.value.bgp.always_compare_med, null)
+  always_compare_med_variable       = try("{{${each.value.bgp.always_compare_med}}}", null)
+  as_number                         = try(each.value.bgp.as_number, null)
+  as_number_variable                = try("{{${each.value.bgp.as_number_variable}}}", null)
+  compare_router_id                 = try(each.value.bgp.compare_router_id, null)
+  compare_router_id_variable        = try("{{${each.value.bgp.compare_router_id_variable}}}", null)
+  description                       = try(each.value.bgp.description, null)
+  deterministic_med                 = try(each.value.bgp.deterministic_med, null)
+  deterministic_med_variable        = try("{{${each.value.bgp.deterministic_med_variable}}}", null)
+  external_routes_distance          = try(each.value.bgp.external_routes_distance, null)
+  external_routes_distance_variable = try("{{${each.value.bgp.external_routes_distance_variable}}}", null)
+  feature_profile_id                = sdwan_service_feature_profile.service_feature_profile[each.value.profile.name].id
+  hold_time                         = try(each.value.bgp.hold_time, null)
+  hold_time_variable                = try("{{${each.value.bgp.hold_time_variable}}}", null)
+  internal_routes_distance          = try(each.value.bgp.internal_routes_distance, null)
+  internal_routes_distance_variable = try("{{${each.value.bgp.internal_routes_distance_variable}}}", null)
+  ipv4_eibgp_maximum_paths          = try(each.value.bgp.ipv4_eibgp_maximum_paths, null)
+  ipv4_eibgp_maximum_paths_variable = try("{{${each.value.bgp.ipv4_eibgp_maximum_paths_variable}}}", null)
+  ipv4_originate                    = try(each.value.bgp.ipv4_originate, null)
+  ipv4_originate_variable           = try("{{${each.value.bgp.ipv4_originate_variable}}}", null)
+  keepalive_time                    = try(each.value.bgp.keepalive_time, null)
+  keepalive_time_variable           = try("{{${each.value.bgp.keepalive_time_variable}}}", null)
+  local_routes_distance             = try(each.value.bgp.local_routes_distance, null)
+  local_routes_distance_variable    = try("{{${each.value.bgp.local_routes_distance_variable}}}", null)
+  missing_med_as_worst              = try(each.value.bgp.missing_med_as_worst, null)
+  missing_med_as_worst_variable     = try("{{${each.value.bgp.missing_med_as_worst_variable}}}", null)
+  multipath_relax                   = try(each.value.bgp.multipath_relax, null)
+  multipath_relax_variable          = try("{{${each.value.bgp.multipath_relax_variable}}}", null)
+  propagate_as_path                 = try(each.value.bgp.propagate_as_path, null)
+  propagate_as_path_variable        = try("{{${each.value.bgp.propagate_as_path_variable}}}", null)
+  propagate_community               = try(each.value.bgp.propagate_community, null)
+  propagate_community_variable      = try("{{${each.value.bgp.propagate_community_variable}}}", null)
+  router_id                         = try(each.value.bgp.router_id, null)
+  router_id_variable                = try("{{${each.value.bgp.router_id_variable}}}", null)
+  ipv4_neighbors           = try(length(each.value.bgp.ipv4_neighbors) == 0, true) ? null : [for ipv4_neighbor in each.value.bgp.ipv4_neighbors : {
+    address                           = try(ipv4_neighbor.address, null)
+    address_variable                  = try("{{${ipv4_neighbor.address_variable}}}", null)
+    allowas_in_number                 = try(ipv4_neighbor.allowas_in_number, null)
+    allowas_in_number_variable        = try("{{${ipv4_neighbor.allowas_in_number_variable}}}", null)
+    as_override                       = try(ipv4_neighbor.as_override, null)
+    description                       = try(ipv4_neighbor.description, null)
+    description_variable              = try("{{${ipv4_neighbor.description_variable}}}", null)
+    ebgp_multihop                     = try(ipv4_neighbor.ebgp_multihop, null)
+    ebgp_multihop_variable            = try("{{${ipv4_neighbor.ebgp_multihop_variable}}}", null)
+    hold_time                         = try(ipv4_neighbor.hold_time, null)
+    hold_time_variable                = try("{{${ipv4_neighbor.hold_time_variable}}}", null)
+    keepalive_time                    = try(ipv4_neighbor.keepalive_time, null)
+    keepalive_time_variable           = try("{{${ipv4_neighbor.keepalive_time_variable}}}", null)
+    local_as                          = try(ipv4_neighbor.local_as, null)
+    local_as_variable                 = try("{{${ipv4_neighbor.local_as_variable}}}", null)
+    next_hop_self                     = try(ipv4_neighbor.next_hop_self, null)
+    next_hop_self_variable            = try("{{${ipv4_neighbor.next_hop_self_variable}}}", null)
+    password                          = try(ipv4_neighbor.password, null)
+    password_variable                 = try("{{${ipv4_neighbor.password_variable}}}", null)
+    remote_as                         = try(ipv4_neighbor.remote_as, null)
+    remote_as_variable                = try("{{${ipv4_neighbor.remote_as_variable}}}", null)
+    send_community                    = try(ipv4_neighbor.send_community, null)
+    send_community_variable           = try("{{${ipv4_neighbor.send_community_variable}}}", null)
+    send_extended_community           = try(ipv4_neighbor.send_extended_community, null)
+    send_extended_community_variable  = try("{{${ipv4_neighbor.send_extended_community_variable}}}", null)
+    send_label                        = try(ipv4_neighbor.send_label, null)
+    send_label_variable               = try("{{${ipv4_neighbor.send_label_variable}}}", null)
+    shutdown                          = try(ipv4_neighbor.shutdown, null)
+    shutdown_variable                 = try("{{${ipv4_neighbor.shutdown_variable}}}", null)
+    update_source_interface           = try(ipv4_neighbor.update_source_interface, null)
+    update_source_interface_variable  = try("{{${ipv4_neighbor.update_source_interface_variable}}}", null)
+    address_families = try(length(ipv4_neighbor.address_families) == 0, true) ? null : [for ipv4_neighbor_af in ipv4_neighbor.address_families : {
+      family_type                       = try(ipv4_neighbor_af.family_type, null)
+      in_route_policy_id                = try(sdwan_service_route_policy_feature.service_route_policy_feature["${each.value.profile.name}-${ipv4_neighbor_af.in_route_policy}"].id, null)
+      max_number_of_prefixes            = try(ipv4_neighbor_af.max_number_of_prefixes, null)
+      max_number_of_prefixes_variable   = try("{{${ipv4_neighbor_af.max_number_of_prefixes_variable}}}", null)
+      out_route_policy_id               = try(sdwan_service_route_policy_feature.service_route_policy_feature["${each.value.profile.name}-${ipv4_neighbor_af.out_route_policy}"].id, null)
+      policy_type                       = try(ipv4_neighbor_af.policy_type, null)
+      restart_interval                  = try(ipv4_neighbor_af.restart_interval, null)
+      restart_interval_variable         = try("{{${ipv4_neighbor_af.restart_interval_variable}}}", null)
+      threshold                         = try(ipv4_neighbor_af.threshold, null)
+      threshold_variable                = try("{{${ipv4_neighbor_af.threshold_variable}}}", null)
+    }]
+  }]
+  ipv4_networks = try(length(each.value.bgp.ipv4_networks) == 0, true) ? null : [for ipv4_net in each.value.bgp.ipv4_networks : {
+    network_address           = try(ipv4_net.network_address, null)
+    network_address_variable  = try("{{${ipv4_net.network_address_variable}}}", null)
+    subnet_mask               = try(ipv4_net.subnet_mask, null)
+    subnet_mask_variable      = try("{{${ipv4_net.subnet_mask_variable}}}", null)
+  }]
+  ipv4_redistributes = try(length(each.value.bgp.ipv4_redistributes) == 0, true) ? null : [for ipv4_redist in each.value.bgp.ipv4_redistributes : {
+    protocol               = try(ipv4_redist.protocol, null)
+    protocol_variable      = try("{{${ipv4_redist.protocol_variable}}}", null)
+    route_policy_id        = try(sdwan_service_route_policy_feature.service_route_policy_feature["${each.value.profile.name}-${ipv4_redist.route_policy}"].id, null)
+  }]
+  ipv4_aggregate_addresses = try(length(each.value.bgp.ipv4_aggregate_addresses) == 0, true) ? null : [for ipv4_aggregate in each.value.bgp.ipv4_aggregate_addresses : {
+    as_set_path              = try(ipv4_aggregate.as_set_path, null)
+    as_set_path_variable     = try("{{${ipv4_aggregate.as_set_path_variable}}}", null)
+    network_address          = try(ipv4_aggregate.network_address, null)
+    network_address_variable = try("{{${ipv4_aggregate.network_address_variable}}}", null)
+    subnet_mask              = try(ipv4_aggregate.subnet_mask, null)
+    subnet_mask_variable     = try("{{${ipv4_aggregate.subnet_mask_variable}}}", null)
+    summary_only             = try(ipv4_aggregate.summary_only, null)
+    summary_only_variable    = try("{{${ipv4_aggregate.summary_only_variable}}}", null)
+  }]
+}
+
+resource "sdwan_service_lan_vpn_feature_associate_routing_bgp_feature" "lan_vpn_feature_associate_routing_bgp_feature" {
+  for_each = {
+    for bgp_association in flatten([
+      for profile in try(local.feature_profiles.service_profiles, []) : [
+        for association in try(profile.bgp_associations, []) : {
+          profile = profile
+          association = association
+        }
+      ]
+    ])
+    : "${bgp_association.profile.name}-${bgp_association.association.service_lan_vpn_feature}-${bgp_association.association.service_routing_bgp_feature}" => bgp_association
+  }
+  feature_profile_id             = sdwan_service_feature_profile.service_feature_profile[each.value.profile.name].id
+  service_lan_vpn_feature_id     = try(sdwan_service_lan_vpn_feature.service_lan_vpn_feature["${each.value.profile.name}-${each.value.association.service_lan_vpn_feature}"].id, null)
+  service_routing_bgp_feature_id = try(sdwan_service_routing_bgp_feature.service_routing_bgp_feature["${each.value.profile.name}-${each.value.association.service_routing_bgp_feature}"].id, null)
+}
